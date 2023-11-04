36839b7b912cbeed9d21149bf23cb7a9c626741c
vincennlin <vincentagwa@gmail.com>
2023-11-04 14:59:38 +0800

    little change

--- src/main/java/com/example/devsecops/DemoApplication.java
+++ src/main/java/com/example/devsecops/DemoApplication.java
@@ -1,7 +1,8 @@
-package com.example.demo;
+package com.example.devsecops;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication; import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
